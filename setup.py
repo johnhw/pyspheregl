540da08cd5f3375cde0f8770aa074b3aed67f5a5
johnhw <johnhw@gmail.com>
2016-01-11 17:19:23 +0000

    Added setup.py and setup.cfg to initial version

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'puffersphere',
+  #packages = ['puffersphere'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'Python/Pyglet',
+  author = 'John Williamson',
+  author_email = 'johnhw@gmail.com',
+  url = 'https://github.com/johnhw/pypuffersphere', # use the URL to the github repo
+  download_url = 'https://github.com/johnhw/pypuffersphere/tarball/0.1',
+  keywords = ['puffersphere', 'sphere', 'touch', 'spherical'], 
+  py_modules=['puffersphere'],
+  classifiers = [],
+)
