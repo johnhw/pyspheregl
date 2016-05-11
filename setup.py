378221fcd635ed54e65cad33c021606e1f1c571a
Andrew Ramsay <andrew.ramsay@gmail.com>
2016-05-11 16:50:42 +0100

    updated setup script with extension, deps, packaging etc

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+from numpy import get_include
+
+sphere_ext = Extension('pypuffersphere/sphere_cy', sources=['pypuffersphere/sphere_cy.pyx'])
+# doesn't work on OSX unless you do this explicitly for some reason...
+sphere_ext.include_dirs = [get_include()]
+
 setup(
-  name = 'puffersphere',
-  #packages = ['puffersphere'], # this must be the same as the name above
-  version = '0.0.1',
-  description = 'Python/Pyglet',
-  author = 'John Williamson',
-  author_email = 'johnhw@gmail.com',
-  url = 'https://github.com/johnhw/pypuffersphere', # use the URL to the github repo
-  download_url = 'https://github.com/johnhw/pypuffersphere/tarball/0.1',
-  keywords = ['puffersphere', 'sphere', 'touch', 'spherical'], 
-  py_modules=['puffersphere'],
-  classifiers = [],
+    # basic info
+    name = 'pypuffersphere',
+    version = '0.0.1',
+    packages = find_packages(),
+    # also pygame, but can't easily install that like this 
+    install_requires = ['numpy', 'pyopengl', 'pyglet', 'Cython', 'pyosc' ],
+
+    # files 
+    ext_modules = cythonize([sphere_ext]),
+    include_package_data = True,
+
+    # metadata
+    description = 'Python/Pyglet code for rendering on the PufferSphere',
+    author = 'John Williamson',
+    author_email = 'johnhw@gmail.com',
+    url = 'https://github.com/johnhw/pypypuffersphere', 
+    keywords = ['pypuffersphere', 'sphere', 'touch', 'spherical'], 
+    classifiers = [],
 )
