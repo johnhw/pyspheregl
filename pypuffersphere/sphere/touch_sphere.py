31d0f23f31f8c4208020adb805964dee5134559c
Andrew Ramsay <andrew.ramsay@gmail.com>
2016-06-06 14:04:59 +0100

    update touch_sphere to allow defining latitude-based touch zones, and returning extended touch data with points assigned to the appropriate zone

--- pypuffersphere/sphere/touch_sphere.py
+++ pypuffersphere/sphere/touch_sphere.py
@@ -4,12 +4,14 @@ import time
 import threading
 import traceback
 
+from sphere import tuio_to_polar
+
 #  Private class for this library
 class touch_sphere():
 
     initiated = False
 
-    def __init__(self, port, ip, logging, fseq, frame_rate):
+    def __init__(self, port, ip, logging, fseq, frame_rate, zones):
         self.ip = ip
         self.port = port
         self.logging = logging
@@ -20,11 +22,13 @@ class touch_sphere():
         self.osc_server = 0 
         self.last_update = 0
         self.frame_rate = frame_rate
+        self.zones = zones
+        self.zonepoints = {}
+        self.reset_points()
         
     def init_touch(self):
         #  Setup OSC Connections
         self.osc_server = OSCServer((self.ip,self.port))
-
 
     def add_handler(self, address, function):
         # bind addresses to functions 
@@ -37,22 +41,33 @@ class touch_sphere():
     def stop_touch(self):
          if self.osc_server is not 0: self.osc_server.close() 
 
+    def reset_points(self):
+        self.touchpoints = {}
+        self.zonepoints = {}
+        for k in self.zones.keys():
+            self.zonepoints[k] = {}
 
 #  Function for Handling TUIO OSC Messages
 def osc_tuio(addr, tags, data, source):
+    global touch_sphere
     if touch_sphere is not None:
-        
-        global touch_sphere
-        
         #  This message denotes the beginning of a touch frame - alive refers to unique touch points
         if data[0] == "alive":
             pass
-        
         #  This message gives a variety of data about a particular touch point
         elif data[0] == "set":
             #print "ID: " , data[1] , " X: " , data[2] , " Y: " , data[3]
             touch_sphere.touchpoints[data[1]] = (data[2], 1.0-data[3])
-            
+            try:
+                lon, lat = tuio_to_polar(data[2], 1.0-data[3])
+                for k in touch_sphere.zones.keys():
+                    zmin, zmax = touch_sphere.zones[k]
+                    if lat >= zmin and lat <= zmax:
+                        #print('Adding point %f,%f to zone %s' % (lon, lat, k))
+                        touch_sphere.zonepoints[k][data[1]] = (data[2], 1.0-data[3], lon, lat) 
+            except Exception as e:
+                print "SET EX", e
+    
         #  Denotes the end of a touch frame, should initiate a drawing    
         elif data[0] == "fseq":
             try:
@@ -67,16 +82,13 @@ def osc_tuio(addr, tags, data, source):
 
                 if this_frame - touch_sphere.last_update > touch_sphere.frame_rate:
                     # print "Adding to touch queue" , this_frame, touch_sphere.last_update, this_frame-touch_sphere.last_update, touch_sphere.frame_rate
-                    touch_sphere.drawing_q.put(touch_sphere.touchpoints)
+                    touch_sphere.drawing_q.put((touch_sphere.touchpoints, touch_sphere.zonepoints))
                     touch_sphere.last_update = this_frame
 
-                touch_sphere.touchpoints = {}
+                touch_sphere.reset_points()
             except Exception as e:
-                # if logging:
-
-                #     #  TODO add error logging code
-
-                    pass
+                print "FSEQ EX", e
+                pass
 
 
 #  Public API for touch Sphere
@@ -86,15 +98,17 @@ def osc_tuio(addr, tags, data, source):
 #  ip address for listening for OSC messages
 #  logging (boolean) turns on error and touch point logging to a text file (not currently implemented)
 #  fseq (boolean) turns on including the frame sequence number in the touchpoint list
-def init(ip, port=3333, logging=False, fseq=False, frame_rate=0):
+def init(ip, port=3333, logging=False, fseq=False, frame_rate=0, zones={}):
     try:
         global touch_sphere
-        touch_sphere=touch_sphere(port, ip, logging, fseq, frame_rate)
+        touch_sphere=touch_sphere(port, ip, logging, fseq, frame_rate, zones)
         touch_sphere.init_touch()
         touch_sphere.initiated = True
-        global last_touches
+        global last_touches, last_zonetouches
         last_touches = {}
-
+        last_zonetouches = {}
+        for k in touch_sphere.zones.keys():
+            last_zonetouches[k] = {}
     except:
         print traceback.print_exc()
 
@@ -106,6 +120,9 @@ def add_handler(address='/tuio/2Dcur', function=osc_tuio):
 
     if touch_sphere.initiated:
         touch_sphere.add_handler(address, function)
+        
+def get_zones():
+    return touch_sphere.zones
 
 def start():
     global touch_sphere
@@ -120,28 +137,26 @@ def stop():
         touch_sphere.stop_touch()
 
 def get_touches():
-    global touch_sphere, last_touches
+    global touch_sphere, last_touches, last_zonetouches
 
     if touch_sphere.initiated:
         try:
             touches = {}
+            zonetouches = {}
 
             # TODO FIX THIS WITH SPHERE
             # while not touch_sphere.drawing_q.empty():
             
-            touches = touch_sphere.drawing_q.get_nowait()
-                # print "Popping Old Touches, fseq=",  touches[-1]
-
-            # print "Returning a touch dict, fseq=", touches[-1] 
+            touches, zonetouches = touch_sphere.drawing_q.get_nowait()
             
             last_touches = touches           
-            return touches
-
-        except:
-            return last_touches
+            last_zonetouches = zonetouches
+            return touches, zonetouches
+        except Exception as e:
+            return last_touches, last_zonetouches
     else:
-        return {}
-
+        return touches, last_zonetouches
+        
 def is_up():
     return touch_sphere.initiated
 
