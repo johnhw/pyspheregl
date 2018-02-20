b233be559d9bcac1dfafd02bf8b93afd234d2c9c
John Williamson <johnhw@gmail.com>
2018-02-20 21:52:35 +0000

    Made calibration use all targets, not just the mean

--- pypuffersphere/demo/test_calibrate.py
+++ pypuffersphere/demo/test_calibrate.py
@@ -1,9 +1,42 @@
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
 from pypuffersphere.touch.touch_calibration import Calibration
 
-if len(sys.argv)==2:
-    calibration = sys.argv[1]
-else:
-    calibration = None # use newest file
+if __name__=="__main__":
 
-calibration = Calibration(calibration)
+    if len(sys.argv)==2:
+        calibration = sys.argv[1]
+    else:
+        calibration = None # use newest file
+
+    calibration = Calibration(calibration)
+
+    cal = calibration.calibration
+    plt.figure()
+    plt.plot(np.degrees(cal["target_lon"]), np.degrees(cal["target_lat"]), 'ro')
+    plt.plot(np.degrees(cal["touch_lon"]), np.degrees(cal["touch_lat"]), 'go')
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.title("Targets")
+    plt.figure()
+    for ix, row in cal.iterrows():
+        lon, lat = row["target_lon"], row["target_lat"]
+        tlon, tlat = row["touch_lon"], row["touch_lat"]
+            
+        
+        lonc, latc = calibration.adjust(tlon, tlat)
+        plt.plot(np.degrees(lon), np.degrees(lat), 'ro')     
+        plt.plot(np.degrees(lonc), np.degrees(latc), 'gx')
+        plt.plot([np.degrees(lonc),np.degrees(tlon)], [np.degrees(latc), np.degrees(tlat)], 'b')
+        
+    lons = np.random.uniform(-np.pi, np.pi, (30,))
+    lats = np.random.uniform(-np.pi/4, np.pi/2, (30,))
+    for lon, lat in zip(lons, lats):
+        lonc, latc = calibration.adjust(lon, lat)
+        plt.plot([np.degrees(lon), np.degrees(lonc)], [np.degrees(lat), np.degrees(latc)], 'b')
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.title("Correction")    
+    plt.show()
+    
--- pypuffersphere/touch/touch_calibration.py
+++ pypuffersphere/touch/touch_calibration.py
@@ -7,27 +7,21 @@ from sklearn import gaussian_process
 from pypuffersphere.sphere import sphere
 
 
-def train_gp(calibration, subsample=None):
+def train_gp(calibration, alpha=1e-2):
     """Train a squared-exponential Gaussian process to predict offsets
     in azimuthal equidistant space. The GPs are trained on x, y, z inputs in the
     cartesian space, and predict x' and y' outputs.
     
     Returns a GP object that performs the prediction."""
-    unique_targets = calibration.groupby(["target_x", "target_y"]).mean()    
+    unique_targets = calibration.copy()
     
-    # subsample if required
-    if subsample is not None:
-        unique_targets = unique_targets.loc[random.sample(unique_targets.index, subsample)]
 
     tx, ty, tz = sphere.spherical_to_cartesian((unique_targets["target_lon"], unique_targets["target_lat"]))
-
     x,y,z = sphere.spherical_to_cartesian((unique_targets["touch_lon"], 
                                             unique_targets["touch_lat"]))    
-
-    
     target = np.vstack((x,y,z)).transpose()       
     residual = np.vstack((tx, ty, tz)).T
-    gp = gaussian_process.GaussianProcessRegressor(alpha=1e-3)
+    gp = gaussian_process.GaussianProcessRegressor(alpha=alpha)
     gp.fit(target, residual)
     return gp
 
@@ -42,7 +36,6 @@ def gp_adjust(lon, lat, gp):
 def fix_angle(x):
     return np.arctan2(np.sin(x), np.cos(x))
 
-
 def augment_calibration(calibration):
     xs, ys = calibration["tuio_x"], calibration["tuio_y"]
     lonlat = np.array([sphere.tuio_to_polar(x,y) for x,y in zip(xs,ys)])    
@@ -53,7 +46,6 @@ def augment_calibration(calibration):
     calibration["target_x"],calibration["target_y"] = xys[:,0], xys[:,1]
 
     # fix angles
-
     calibration["target_lon"] = fix_angle(calibration["target_lon"])
     calibration["touch_lon"] = fix_angle(calibration["touch_lon"])
     
@@ -76,8 +68,6 @@ def get_calibrated_touch(tuio_x, tuio_y, gp):
     lon,lat = gp_adjust(lon, lat, gp)
     return lon, lat
     
-
-
 def error_distribution(calibration, fn):
     """Return the error distribution after applying fn"""
     ds = []
@@ -87,20 +77,9 @@ def error_distribution(calibration, fn):
         
         # compute offset from constant corrected
         lonc, latc = fn(tlon, tlat)
-        # plt.plot(lon, lat, 'ro')     
-        # plt.plot(lonc, latc, 'gx')
-        # plt.plot([lonc,tlon], [latc, tlat], 'b')
         d = sphere.spherical_distance((lon, lat), (lonc, latc))
         ds.append(np.degrees(d))      
 
-    # lons = np.random.uniform(-np.pi, np.pi, (30,))
-    # lats = np.random.uniform(-np.pi/4, np.pi/2, (30,))
-    # for lon, lat in zip(lons, lats):
-    #     lonc, latc = fn(lon, lat)
-    #     plt.plot([lon, lonc], [lat, latc], 'b')
-        
-        
-    # plt.show()
     return ds          
 
 def rms(x):
@@ -109,11 +88,14 @@ def rms(x):
 class CalibrationException(Exception):
     pass
 
-# import matplotlib.pyplot as plt
+CALIBRATION_NOISE_LEVEL = 1e-2
 
 class Calibration(object):
     def get_calibrated_touch(self, x, y):
         return get_calibrated_touch(x,y,self.gp)
+
+    def adjust(self, lon, lat):
+        return gp_adjust(lon, lat, self.gp)
         
     def __init__(self, calibration_name=None, exclude_distance=25):
         if calibration_name is None:
@@ -162,14 +144,12 @@ class Calibration(object):
         self.min_latitude = np.min(calibration["target_lat"]) - np.radians(5)
 
 
-        # plt.plot(calibration["target_lon"], calibration["target_lat"], 'ro')
-        # plt.plot(calibration["touch_lon"], calibration["touch_lat"], 'go')
-        # plt.show()
+        self.calibration = calibration
 
-        self.gp = train_gp(calibration)       
+        self.gp = train_gp(calibration, alpha=CALIBRATION_NOISE_LEVEL)       
         error = error_distribution(calibration, lambda x,y:gp_adjust(x,y,self.gp))
 
-        # plt.show()
+     
         self.rms_error = rms(error)
         self.median_error =  np.median(error)
         
