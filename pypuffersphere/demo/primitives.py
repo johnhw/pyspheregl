Fixed calibration bug and added smooth rotation manager

pypuffersphere/demo/primitives.py
@@ -10,14 +10,19 @@ from pypuffersphere.utils.shader import ShaderVBO, shader_from_file
 
 import time
 from pypuffersphere.utils.np_vbo import VBuf, IBuf
-from pypuffersphere.utils.graphics_utils import make_unit_quad_tile
+from pypuffersphere.utils.graphics_utils import make_unit_quad_tile, make_circle_fan
+import pypuffersphere.utils.transformations as tn
+
+    
 
 class Primitives(object):
     def __init__(self):
         self.viewer = sphere_sim.make_viewer(show_touches=True, draw_fn=self.draw, touch_fn=self.touch)
 
-        refs = {'npole':[0, np.pi/2], 'spole':[0,-np.pi/2], 'gmq':[0,0], 'wmq':[np.pi/2,0], 'emq':[-np.pi/2,0], 'rmq':[np.pi,0]}
-        colors = {'npole':[0,0,1,1], 'spole':[1,0,0,1], 'gmq':[0,1,0.5,1], 'rmq':[0.5,0,1,1], 'emq':[1,1,0,1], 'wmq':[0,1,1,1]}
+
+        refs = {'npole':[0.0, np.pi/2-0.0001], 'spole':[0,-np.pi/2], 'gmq':[0,0], 'wmq':[np.pi/2,0], 'emq':[-np.pi/2,0], 'rmq':[np.pi,0]}
+        colors = {'npole':[0,0,1,1], 'spole':[1,0,0,1], 'gmq':[0,1,0.5,1], 
+        'rmq':[0.5,0,1,1], 'emq':[1,1,0,1], 'wmq':[0,1,1,1]}
 
         ks = refs.keys()
         pts = np.array([refs[k] for k in ks], dtype=np.float32)
@@ -44,19 +49,36 @@ class Primitives(object):
         # create a subdivided quad to be drawn
         ixs, quad, texs = make_unit_quad_tile(64)
         
+        
         # grid shader, across the whole sphere
         whole_shader = shader_from_file([getshader("sphere.vert"), getshader("user/whole_sphere.vert")], [getshader("user/whole_sphere_grid.frag")])        
         self.whole_vbo = ShaderVBO(whole_shader, IBuf(ixs), 
                             buffers={"quad_vtx": VBuf(quad)})
 
 
+       
+
         # simple flat quad shader
         quad_shader = shader_from_file([getshader("sphere.vert"), getshader("user/quad.vert")], [getshader("user/quad_color.frag")])               
         self.quad_vbo = ShaderVBO(quad_shader, IBuf(ixs), 
                             buffers={"quad_vtx": VBuf(quad, divisor=0),
                                     "position":VBuf(pts, divisor=1)}, 
-                            attribs={"fcolor":(0.5, 1.0, 0.2, 0.25)},
+                            attribs={"fcolor":(0.5, 1.0, 0.2, 0.25),
+                                    "up_vector":(0.0,0.0,1.0)},
                             vars={"scale":0.5})
+
+        
+        # simple circle shader    
+        # uses same shader as the quads
+        # works for any planar polygon
+        circle_ixs, circle_verts = make_circle_fan(64)   
+        self.circle_vbo = ShaderVBO(quad_shader, IBuf(circle_ixs), 
+                            buffers={"quad_vtx": VBuf(circle_verts, divisor=0),
+                                    "position":VBuf(pts, divisor=1)}, 
+                            attribs={"fcolor":(0.5, 1.0, 0.2, 0.25)},
+                            vars={"scale":0.5}, primitives=GL_TRIANGLE_FAN)
+
+
 
         # whole sphere gradient, from an image texture
         grad_shader = shader_from_file([getshader("sphere.vert"), getshader("user/whole_sphere.vert")], [getshader("user/whole_sphere_gradient.frag")])
@@ -87,6 +109,7 @@ class Primitives(object):
         self.whole_vbo.draw()
         self.point_vbo.draw()
         self.line_vbo.draw()
+        self.circle_vbo.draw(n_prims=8)
         
         
         

pypuffersphere/touch/touch_calibration.py
@@ -5,7 +5,7 @@ import pickle
 import os, sys, time, random
 from sklearn import gaussian_process
 from pypuffersphere.sphere import sphere
-
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 
 def train_gp(calibration, alpha=1e-2):
     """Train a squared-exponential Gaussian process to predict offsets
@@ -20,15 +20,17 @@ def train_gp(calibration, alpha=1e-2):
                                             unique_targets["touch_lat"]))    
     target = np.vstack((x,y,z)).transpose()       
     residual = np.vstack((tx, ty, tz)).T
+    
     gp = gaussian_process.GaussianProcessRegressor(alpha=alpha)
     gp.fit(target, residual)
+   
     return gp
 
         
 def gp_adjust(lon, lat, gp):
     x,y,z = sphere.spherical_to_cartesian((lon, lat))
     res = gp.predict([[x,y,z]])
-    xc, yc, zc = res[0] + np.array([x,y,z])
+    xc, yc, zc = res[0]
     corr_touch_lon, corr_touch_lat = sphere.cart_to_polar(xc, yc, zc)
     return corr_touch_lon, corr_touch_lat
 

pypuffersphere/utils/graphics_utils.py
@@ -53,7 +53,23 @@ def load_image_and_fit(img_string, sz):
     image = Image.open(StringIO(img_string))
     image = fit_image(image, sz)        
     return pyglet.image.ImageData(image.width, image.height, 'RGB', image.tobytes(), pitch=-image.width * 3).get_mipmapped_texture()
-    
+
+
+def make_circle_fan(n_divs, radius=1.0):
+    # make a TRIANGLE_FAN style circle
+    vertices = [[0,0]]
+    indices = [0]
+    for i in range(n_divs):
+        angle = (i/float(n_divs)) * 2 * np.pi
+        x, y = np.cos(angle), -np.sin(angle)
+        vertices.append((x,y))
+        indices.append(i+1)
+    # end point
+    x, y = np.cos(0), -np.sin(0)        
+    vertices.append((x,y))
+    indices.append(n_divs+1)
+    return np.array(indices, dtype=np.uint32), np.array(vertices, dtype=np.float32)
+
     
 def make_unit_quad_tile(n_divs, x1=0.0, x2=1.0, y1=0.0, y2=1.0, tx1=0.0, tx2=1.0, ty1=0.0, ty2=1.0, n_quads=1):
     # subdivide a rectangle into n_divs x n_divs smaller sub-rectangles, with corresponding texture coordinates