41ab581c02a5a638303ca252a9225eb7c852908b
Joseph O'Hagan <josephaohagan@gmail.com>
2018-09-27 15:19:55 +0100

    Updated align_test_v2 and added search equivalent example

--- pyspheregl/demo/search_test.py
+++ pyspheregl/demo/search_test.py
@@ -16,7 +16,7 @@ from ..utils import transformations as tn
 
 from ..touch.rotater import RotationHandler
 
-class AlignTest(object):
+class SearchTest(object):
     spiral_amount = 256                                                             # Number of spirals to generate
     target_distance = 99                                                            # Initial dummy value for target distance
 
@@ -27,11 +27,11 @@ class AlignTest(object):
     time_within_ring = 3                                                            # Number of seconds to align dot within ring
 
     align_n = 31                                                                    # Dot position to display task dot    
-    target_pt = np.array([[np.radians(180), np.radians(0)]], dtype=np.float32)       # Position of target on sphere
-    
-    reset = False
-    flag = False
+    target_pt = np.array([[np.radians(180), np.radians(0)]], dtype=np.float)       # Position of target on sphere
 
+    reset = False
+    touched_point = False
+    complete = False
 
     # Generate a new random dot position from spiral amount
     def generate_random_dot_pt(self):
@@ -39,6 +39,17 @@ class AlignTest(object):
         while t == self.align_n:
             t = randint(0, self.spiral_amount-1)
         return t
+
+
+    # Check if two points are within some threshold degree of each other
+    def check_pts(self, xyz, rotated_origin):    
+        if (np.degrees(self.target_distance) < self.target_threshold and np.degrees(self.target_distance) > (-1) * self.target_threshold):            
+            return True
+        else:
+            return False
+
+    def xyz_to_lonlat(self, xyz):
+        return sphere.cart_to_polar(xyz[0], xyz[1], xyz[2])
 
 
     def __init__(self):
@@ -56,7 +67,8 @@ class AlignTest(object):
         self.align_pts = self.pts[self.align_n : self.align_n + 1, :]
 
         self.origin = sphere.spherical_to_cartesian(self.pts[self.align_n])             # The point that we start at        
-        self.target = sphere.spherical_to_cartesian(self.target_pt[0])                  # The point we want to align with
+
+        self.finger_pts = np.array([[0.0, 0.0]], dtype=np.float)
 
         # Point shader; simple coloured circles, with no spherical correction
         point_shader = shader_from_file([getshader("sphere.vert"), getshader("user/point.vert")], [getshader("user/point.frag")])
@@ -74,28 +86,33 @@ class AlignTest(object):
                                     vars={"constant_size":20.0,                                    
                                     },                                     
                                     primitives=GL_POINTS)
-
-
-        # Target (ring) shader and VBO
-        target_point_shader = shader_from_file([getshader("sphere.vert"), getshader("user/ring.vert")], [getshader("user/ring.frag")])        
-        self.target_point_vbo = ShaderVBO(target_point_shader, IBuf(np.arange(len(self.target_pt))), 
-                                    buffers={"position":VBuf(self.target_pt), },
-                                    vars={"constant_size":30.0,   
-                                    "inner_size":20.0,                                 
-                                    },                                     
-                                    primitives=GL_POINTS)
         
         self.viewer.start()
 
     def touch(self, events):        
         for event in events:
             xyz = sphere.polar_to_cart(*event.touch.lonlat)
+            
             if event.event=="DRAG":
                 self.rotater.drag(event.touch.id, xyz)
+            
             if event.event=="UP":
                 self.rotater.up(event.touch.id, xyz)
+
+                if not self.complete:
+                    self.end_threshold = datetime.datetime.now() + datetime.timedelta(seconds=360000)
+                    self.point_colour = (1.0, 0.0, 0.0, 1.0)
+                self.touched_point = False
+            
             if event.event=="DOWN":
                 self.rotater.down(event.touch.id, xyz)
+                
+                self.finger_pts = np.array([self.xyz_to_lonlat(xyz)], dtype=np.float)
+
+                if (self.check_pts(xyz, self.rotated_origin)):                    
+                    self.end_threshold = datetime.datetime.now() + datetime.timedelta(seconds=2)                    
+                    self.touched_point = True
+                    self.point_colour = (0.0, 1.0, 0.0, 1.0) 
 
 
     def draw(self):
@@ -114,79 +131,61 @@ class AlignTest(object):
         self.align_point_vbo.draw(vars={"quat":self.rotater.orientation})
         # self.align_point_vbo.draw()                                                                           # Modification
 
-        # Draw the (fixed) target point w/o rotation
-        self.target_point_vbo.set_attrib("color", (0.0, 0.0, 1.0, 1.0))        
-        self.target_point_vbo.draw()
-
         # Compute distance between the points (must be in Cartesian space)
         quat = self.rotater.orientation        
         
         self.rotated_origin = sphere.rotate_cartesian(quat/np.linalg.norm(quat), np.array(self.origin))         # rotate the points with the quaternion                        
         
         # Compare distance, again directly in Cartesian space
-        self.target_distance = sphere.spherical_distance_cartesian(self.rotated_origin, self.target)       
+
+        self.target_distance = sphere.spherical_distance(sphere.cart_to_polar(self.rotated_origin[0], self.rotated_origin[1], -self.rotated_origin[2]) , self.finger_pts[0])
+
+        # self.target_distance = sphere.spherical_distance_cartesian(self.rotated_origin, self.target)       
 
     def tick(self):        
         self.rotater.update(1/60.0)
 
-        # If within target ring                
-        if (np.degrees(self.target_distance) < self.target_threshold and np.degrees(self.target_distance) > (-1) * self.target_threshold and not self.flag):        
+        if (datetime.datetime.now() >= self.end_threshold and self.touched_point):
+            self.touched_point = False
+            self.complete = True
 
-            # Start the timer for how long to remain within target ring                    
-            if (self.threshold_flag):
-                self.threshold_flag = False            
-                self.end_threshold = datetime.datetime.now() + datetime.timedelta(seconds=self.time_within_ring)
-                self.point_colour = (0.0, 1.0, 0.0, 1.0)
-
-            # Within target for valid time  
-            if datetime.datetime.now() >= self.end_threshold: 
-                self.background_colour = (0.0, 1.0, 0.0, 1.0)
-                self.reset = True
-                self.flag = True                                                 
-        else:
-            # Outside of threshold so reset flag
-            self.threshold_flag = True                        
-            if (np.degrees(self.target_distance) < self.target_threshold and np.degrees(self.target_distance) > (-1) * self.target_threshold):
-                self.point_colour = (0.0, 1.0, 0.0, 1.0)    
-            else:
-                self.point_colour = (1.0, 0.0, 0.0, 1.0)    
-
-
-        # Iteration completed so set time beteen iterations time (i.e. wait 5 seconds until reset and start next iteration)       
+            # Change background dots to indicate success
+            self.background_colour = (0.0, 1.0, 0.0, 1.0)            
+            self.reset = True
+        
+        # Set and start timer between iterations
         if self.reset:
             self.end_threshold = datetime.datetime.now() + datetime.timedelta(seconds=5)
             self.reset = False
 
-
-        # Reset and start next iteration
-        if datetime.datetime.now() >= self.end_threshold and self.flag:
-            # Reset misc. dummy values and flags
-            self.target_distance = 999   
-            self.end_threshold = datetime.datetime.now() + datetime.timedelta(seconds=360000)                       
-            self.flag = False 
-
-            # Reset orientation and colours
-            self.rotater.orientation = np.array([0,0,0,1], dtype=np.float)
-            self.rotater.angular_velocity = np.array([0,0,0,0.0], dtype=np.float)            
+            # Start next iteration
+        if datetime.datetime.now() >= self.end_threshold:                        
+            # Reset flags and dummy value
+            self.touched_point = False
+            self.complete = False
+            self.end_threshold = datetime.datetime.now() + datetime.timedelta(seconds=360000)
+                
+                # Reset colours and orientation
             self.background_colour = (1.0, 1.0, 1.0, 1.0)
             self.point_colour = (1.0, 0.0, 0.0, 1.0)
-
-            # Get next target point
+            self.rotater.orientation = np.array([0,0,0,1], dtype=np.float)
+            self.rotater.angular_velocity = np.array([0,0,0,0.0], dtype=np.float) 
+            
+                # Get next target point
             self.align_n = self.generate_random_dot_pt()               
             self.align_pts = self.pts[self.align_n : self.align_n + 1, :]
             self.origin = sphere.spherical_to_cartesian(self.pts[self.align_n])
 
-            # Draw next target point                    
             align_point_shader = shader_from_file([getshader("sphere.vert"), getshader("user/point.vert")], [getshader("user/point.frag")])        
             self.align_point_vbo = ShaderVBO(align_point_shader, IBuf(np.arange(len(self.align_pts))), 
                                 buffers={"position":VBuf(self.align_pts), },
                                 vars={"constant_size":20.0,                                    
                                 },                                     
                                 primitives=GL_POINTS)
-        pass                
+    pass
               
 if __name__=="__main__":
-    p = AlignTest()
+    p = SearchTest()
     
     
    
